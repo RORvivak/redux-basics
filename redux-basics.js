9c10d394fb6cde3ce3c581658c179d44620949bb
vivak <vivak.k@philips.com>
2019-07-16 07:05:00 +0530

    adding of subscribers

--- redux-basics.js
+++ redux-basics.js
@@ -1,15 +1,23 @@
-const redux = require("redux");
-const createStore = redux.createStore;
-const initialState = { counter: 0 }
+const redux = require("redux")
+const createStore = redux.createStore
+const intitialstate = { counter: 0 }
 
- const rootreducer = (state = initialState, action) => {
-     if(action.type === "ADD_COUNTER")
-     {
-        return ({...state, counter: state.counter +  action.value})
-     }
- }
+const rootreducer = (state = intitialstate, action) => {
+    if(action.type === "ADD_INCREM")
+    {
+        return(
+            {...state,
+            counter: state.counter + action.value}
+        )
+    }
+}
 
-const store = createStore(rootreducer);
-store.dispatch({type: "ADD_COUNTER", value: 5});
 
-console.log(store.getState());
+const store = createStore(rootreducer)
+store.subscribe(()=>{
+    console.log("subscribe", store.getState())
+}
+)
+console.log(store.getState())
+store.dispatch({type: "ADD_INCREM", value: 5})
+console.log(store.getState())
